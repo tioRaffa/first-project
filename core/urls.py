bfa0ecdbff967ec0828549f22f020af703816663
Rafael Muniz <rafaelmuniz200@gmail.com>
2025-02-01 19:20:37 -0300

    praticando shell e entendendo rotas

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, contato, index2
+from .views import index, contato, index2, produto
 
 
 urlpatterns = [
     path('', index),
     path('contato', contato),
-    path('index2', index2)
+    path('index2', index2),
+    path('produto/<int:id>', produto, name='produto')
 ]
--- core/views.py
+++ core/views.py
@@ -1,19 +1,34 @@
 from django.shortcuts import render
-
+from .models import Produto
 # Create your views here.
 
 
 def index(request):
+    # print(dir(request.user))
+    print()
+    # print(f'METODO {request.user.username}, {request.user.password}')
+
     return render(request, 'index.html')
 
 
 def index2(request):
+    produtos = Produto.objects.all()
+
     context = {
         'curso': 'Programacao WEB!',
-        'teste': 'Django framework'
+        'teste': 'Django framework',
+        'produto': produtos,
     }
     return render(request, 'index2.html', context)
 
 
 def contato(request):
     return render(request, 'contato.html')
+
+
+def produto(request, id):
+    produto_ = Produto.objects.get(id=id)
+    context = {
+        'produtu': produto_
+    }
+    return render(request, 'produto.html', context)
